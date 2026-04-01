8de89b9f24478bb66d6d435f87a30bfa84de16bc
adenzhou1350 <aden1350@outlook.com>
2026-04-01 15:06:12 +0800

    remove ;

--- examples/chassis_control.py
+++ examples/chassis_control.py
@@ -108,7 +108,7 @@ def move_by_map(robot: Robot):
     result = robot.chassis.set_trajectory_coord_system_mode(coord_system_mode)
     print(f"set trajectory coord system mode success: {result.is_success}")
 
-    result = robot.navigation.start_mapping();
+    result = robot.navigation.start_mapping()
     print(f"start mapping success: {result.is_success}")
 
     print(f"move around to build map...")
